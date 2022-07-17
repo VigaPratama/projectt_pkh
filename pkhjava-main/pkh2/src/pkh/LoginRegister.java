931e914fc4098b5adb2258363447c73772d710c7
VigaPratama <viga.pratama@gmail.com>
2022-07-17 08:28:13 +0700

    first commit

---
+++ pkhjava-main/pkh2/src/pkh/LoginRegister.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkh;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Me
+ */
+public class LoginRegister {
+     Connection conn =null;
+     Statement st = null;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
